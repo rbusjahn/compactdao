5588ec72d0e93af947528d7606525cd3562b4e69
ruediger busjahn <ruediger.busjahn@3d-berlin.com>
2017-07-05 14:20:32 +0200

    bug fix new version

--- src/main/java/org/rbusjahn/compactdao/DatabaseUpdateRunner.java
+++ src/main/java/org/rbusjahn/compactdao/DatabaseUpdateRunner.java
@@ -3,8 +3,6 @@ package org.rbusjahn.compactdao;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -56,41 +54,15 @@ public class DatabaseUpdateRunner {
 
 	}
 
-	protected List<DatabaseUpdate> getOpenUpdates(List<DatabaseUpdate> appliedUpdates, List<DatabaseUpdate> updates) {
-		List<DatabaseUpdate> list = new ArrayList<>();
-		for (DatabaseUpdate newUpdate : updates) {
-			boolean foundById = false;
-			for (DatabaseUpdate existingUpdate : appliedUpdates) {
-				if (newUpdate.getId().equals(existingUpdate.getId())) {
-					foundById = true;
-					break;
-				}
-			}
-			if (!foundById) {
-				list.add(newUpdate);
-			}
-		}
-		return list;
-	}
-
 	public void runDatabaseUpdates(List<DatabaseUpdate> updates, List<DatabaseUpdate> failedUpdates) {
 		if (updates == null || updates.isEmpty()) {
 			return;
 		}
-		Collections.sort(updates, new Comparator<DatabaseUpdate>() {
-			@Override
-			public int compare(DatabaseUpdate o1, DatabaseUpdate o2) {
-				return o1.getId().compareTo(o2.getId());
-			}
-		});
 
 		final List<DatabaseUpdate> appliedUpdates = dao.findAll();
-		final int updatesLeft = updates.size() - appliedUpdates.size();
-		if (updatesLeft > 0) {
-			final int offset = updates.size() - updatesLeft;
-			for (int i = offset; i < updates.size(); i++) {
-				final DatabaseUpdate update = updates.get(i);
-
+		List<DatabaseUpdate> openUpdates = getOpenUpdates(appliedUpdates, updates);
+		if (openUpdates.size() > 0) {
+			for (DatabaseUpdate update : openUpdates) {
 				try {
 					log.debug("running update: " + update);
 
@@ -117,5 +89,22 @@ public class DatabaseUpdateRunner {
 			throw new RuntimeException(e.getMessage(), e);
 		}
 
+	}
+
+	protected List<DatabaseUpdate> getOpenUpdates(List<DatabaseUpdate> appliedUpdates, List<DatabaseUpdate> updates) {
+		List<DatabaseUpdate> list = new ArrayList<>();
+		for (DatabaseUpdate newUpdate : updates) {
+			boolean foundById = false;
+			for (DatabaseUpdate existingUpdate : appliedUpdates) {
+				if (newUpdate.getId().equals(existingUpdate.getId())) {
+					foundById = true;
+					break;
+				}
+			}
+			if (!foundById) {
+				list.add(newUpdate);
+			}
+		}
+		return list;
 	}
 }
